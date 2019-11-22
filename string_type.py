b66fbcac5bca2b17308f72e818bd6217e2fc0bc5
mchin1 <mchin1@alumni.cmu.edu>
2019-11-22 10:54:58 -0500

    New save

--- string_type.py
+++ string_type.py
@@ -11,3 +11,13 @@ print(s*2) # doubles the string
 print(s*3) # triples the string
 
 print(len(s1))
+
+# Slicing a string
+print(s[0:5])
+print(s[0:])
+print(s[:8])
+print(s[-3:-1])   # -1 is always the last element
+
+print(s[0:9:2])   # ([start,end,step increment]).
+print(s[15::-1])  # step increment can be negative
+print(s[::-1])    # this easily REVERSES a string!
