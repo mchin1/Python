4b77863a3a2f8ad3a890dea803e54c9664e483a6
mchin1 <mchin1@alumni.cmu.edu>
2019-11-22 10:47:20 -0500

    Moving from local file to github

---
+++ hello.py
@@ -0,0 +1,21 @@
+""" Author is Michael Chin
+Created on November 22, 2019 """
+
+# this is my first python program
+
+print("python is easy!!")
+
+
+"""
+Python has 5 data types:
+1. None type - an object that does not contain any value
+2. Numeric type - int, float, complex
+3. Sequences - str, bytes, bytearray, list, tuple, range
+4. Sets
+5. Mappings
+
+Python has 3 collections (data structures):
+1. List - can store any number of objects and values dynamically, and maintains the order
+2. Set - cannot store duplicates
+3. Dictionary - a map (store a Key,Value pair)
+"""
