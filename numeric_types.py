3fa57486b709dc7d75d33ba503d04ad6a5d1d876
mchin1 <mchin1@alumni.cmu.edu>
2019-11-22 10:49:09 -0500

    Moving from local to github

---
+++ numeric_types.py
@@ -0,0 +1,37 @@
+a = 13
+b = 100
+c = -66
+print(a,b,c)
+print(type(a))
+
+x = 33.5
+y = -25.8
+z = 205
+print(z,y,x)
+
+# Complex type
+d = 3 + 5j
+print(d, type(d))
+
+# Binary
+e = 0B1010
+print(e, type(e))
+
+# Hexadecimal
+f = 0XFF
+print(f, type(f))
+
+# Boolean
+g = True
+print(g, type(g))
+print(9>8)
+
+# Type conversion
+h = int(x)      # from x = 33.5 value above
+print(h,type(h))
+
+i = float("22.5")
+print(i,type(i))
+
+print(bin(10)) # converts to binary value
+print(hex(10)) # converts to hex value
---
+++ string_type.py
@@ -0,0 +1,13 @@
+s = "You are awesome"
+print(s)
+
+s1 = """You are
+the creator
+of your destiny"""
+print(s1)
+
+print(s[2]) # 2 index of string s
+print(s*2) # doubles the string
+print(s*3) # triples the string
+
+print(len(s1))
